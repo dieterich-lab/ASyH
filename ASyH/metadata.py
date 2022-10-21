fd9602538672144c837d707c118d3d9771c9cc9d
Tim Johann <tim.johann@phrogstar.de>
2022-10-22 01:39:15 +0200

    metadata.py:  A class for metadata read-in (and preparation of input)
      Metadata should hold a json definition of the table columns' data types (as
      specified by SDV).
      If the given input file does not exist, create a skeleton with column names
      and possible SDV data definitions.

---
+++ ASyH/metadata.py
@@ -0,0 +1,45 @@
+# ASyH's simple metadata inference
+
+import pathlib
+import utils
+import json
+
+
+class Metadata:
+
+    # metadata generally is a dict:
+    metadata = {}
+
+    def _skeleton(self, data):
+        return
+
+    def _create_skeleton_file(self, data):
+        return
+
+    def read(self, filename):
+        '''Read the metadata from file into the metadata dict.'''
+        if pathlib.Path(filename).is_file():
+            with open(filename, 'r', encoding='utf-8') as f:
+                self.metadata = json.load(f)
+        else:
+            raise FileNotFoundError
+
+    def save(self, outfilename):
+        '''Save the metadata dict to json file.'''
+        # (later: +annotating the possible SDV type)
+        with open(outfilename, 'w', encoding='utf-8') as f:
+            json.dump(self.metadata, f)
+
+    def __init__(self, data=None):
+        # for now just use the dataFrame's dtypes:
+        if data is not None:
+            self.metadata = data.dtypes.to_dict()
+
+    def _infer(self, data_column):
+        # ToDo!
+        # for now, just return the dtype
+        return data_column.dtype
+
+    def _infer_metadata(self, data_frame):
+        meta = {x: self._infer(data_frame[x]) for x in data_frame.columns}
+        return meta
