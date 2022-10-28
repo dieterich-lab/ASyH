461cf3f4607d65a6ef52b175777b77d48792e979
Tim Johann <tim.johann@phrogstar.de>
2022-10-28 22:36:32 +0200

    dispatch.py: Proper concurrent execution and return val capture.
      Using multiprocessing.Processing for execution.
    
      multiprocessing.Value is needed for sharing the subprocesses' return values
      with the parent process.

--- ASyH/dispatch.py
+++ ASyH/dispatch.py
@@ -1,13 +1,26 @@
 # ASyH concurrent/async dispatch
-# ToDos:
-#   Joining and collecting return values.
-#
-#   Module docstring
+# ToDos: -
 
-import asyncio
+from multiprocessing import Process, Value
+
+
+def task(pipeline, retval):
+    retval.value = pipeline.run()
 
 
 def concurrent_dispatch(*pipelines):
-    '''Run several ASyH pipelines concurrently with asyncio.'''
-    for p in pipelines:
-        asyncio.run(p.p_run())
+    '''Run several ASyH pipelines concurrently with multiprocessing.Process.'''
+    results = []
+    procs = []
+
+    for i in range(len(pipelines)):
+        ret = Value('f', 0.0)
+        p = Process(target=task, args=(pipelines[i], ret))
+        results.append(ret)
+        procs.append(p)
+        p.start()
+
+    for p in procs:
+        p.join()
+
+    return [results[i].value for i in range(len(pipelines))]
