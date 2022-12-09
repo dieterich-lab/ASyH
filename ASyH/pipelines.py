227185d1c1168f9aad34b383a86156a59addb6e7
Tim Johann <tim.johann@phrogstar.de>
2022-12-10 00:37:27 +0100

    pipelinees.py: added keywords in args.

--- ASyH/pipelines.py
+++ ASyH/pipelines.py
@@ -7,22 +7,30 @@ from ASyH.models import CopulaGANModel, CTGANModel, GaussianCopulaModel, TVAEMod
 class CopulaGANPipeline(Pipeline):
 
     def __init__(self, input_data):
-        Pipeline.__init__(CopulaGANModel(data=input_data), input_data)
+        Pipeline.__init__(self,
+                          model=CopulaGANModel(data=input_data),
+                          input_data=input_data)
 
 
 class CTGANPipeline(Pipeline):
 
     def __init__(self, input_data):
-        Pipeline.__init__(CTGANModel(data=input_data), input_data)
+        Pipeline.__init__(self,
+                          model=CTGANModel(data=input_data),
+                          input_data=input_data)
 
 
 class GaussianCopulaPipeline(Pipeline):
 
     def __init__(self, input_data):
-        Pipeline.__init__(GaussianCopulaModel(data=input_data), input_data)
+        Pipeline.__init__(self,
+                          model=GaussianCopulaModel(data=input_data),
+                          input_data=input_data)
 
 
 class TVAEPipeline(Pipeline):
 
     def __init__(self, input_data):
-        Pipeline.__init__(TVAEModel(data=input_data), input_data)
+        Pipeline.__init__(self,
+                          model=TVAEModel(data=input_data),
+                          input_data=input_data)
