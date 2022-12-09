56abee2d4f44635b18070602a77d69362b1d159b
Tim Johann <tim.johann@phrogstar.de>
2022-12-10 00:24:45 +0100

    models.py:  correct use of metadata, explicit keyword arguments.

--- ASyH/models.py
+++ ASyH/models.py
@@ -11,10 +11,11 @@ class TVAEModel(Model):
 
     def __init__(self, data=None):
         if data:
-            sdv_model = sdv.tabular.TVAE(field_types=data.metadata())
+            sdv_model = sdv.tabular.TVAE(field_types=data.metadata().metadata)
         else:
             sdv_model = sdv.tabular.TVAE()
-        Model.__init__(self, 'TVAE', sdv_model, data)
+        Model.__init__(self, model_type='TVAE',
+                       sdv_model=sdv_model, data=data)
 
     def adapt(self):
         '''Method to adapt the TVAE sdv model internals to data'''
@@ -25,10 +26,11 @@ class CTGANModel(Model):
 
     def __init__(self, data=None):
         if data:
-            sdv_model = sdv.tabular.CTGAN(field_types=data.metadata())
+            sdv_model = sdv.tabular.CTGAN(field_types=data.metadata().metadata)
         else:
             sdv_model = sdv.tabular.CTGAN()
-        Model.__init__(self, 'CTGAN', sdv_model, data)
+        Model.__init__(self, model_type='CTGAN',
+                       sdv_model=sdv_model, data=data)
 
     def adapt(self):
         '''Method to adapt the CTGAN sdv model internals to data'''
@@ -38,10 +40,11 @@ class CopulaGANModel(Model):
 
     def __init__(self, data=None):
         if data:
-            sdv_model = sdv.tabular.CopulaGAN(field_types=data.metadata())
+            sdv_model = sdv.tabular.CopulaGAN(field_types=data.metadata().metadata)
         else:
             sdv_model = sdv.tabular.CopulaGAN()
-        Model.__init__(self, 'CopulaGAN', sdv_model, data)
+        Model.__init__(self, model_type='CopulaGAN',
+                       sdv_model=sdv_model, data=data)
 
     def adapt(self):
         '''Method to adapt the CopulaGAN sdv model internals to data'''
@@ -51,10 +54,11 @@ class GaussianCopulaModel(Model):
 
     def __init__(self, data=None):
         if data:
-            sdv_model = sdv.tabular.GaussianCopula(field_types=data.metadata())
+            sdv_model = sdv.tabular.GaussianCopula(field_types=data.metadata().matadata)
         else:
             sdv_model = sdv.tabular.GaussianCopula()
-        Model.__init__(self, 'GaussianCopula', sdv_model, data)
+       Model.__init__(self, model_type='GaussianCopula',
+                      sdv_model=sdv_model, data=data)
 
     def adapt(self):
         '''Method to adapt the Gaussian Copula sdv model internals to data'''
