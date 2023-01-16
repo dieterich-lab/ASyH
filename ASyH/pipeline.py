5c95317589ef9fa5a2ff13543eafecdb9212551f
Tim Johann <tim.johann@phrogstar.de>
2023-01-16 13:41:40 +0100

    pipeline.py:  single return value for the pipeline.
      From evaluating the functions in the scoring hook we get a dict
      of values.  The scoring step now averages the metric results.
    
      This works best when the scoring functions are maximizing,
      normalized and wheighted equally.

--- ASyH/pipeline.py
+++ ASyH/pipeline.py
@@ -12,6 +12,10 @@ class Pipeline(AbstractPipeline):
 
     _scoring_hook = ScoringHook()
 
+    @property
+    def model(self):
+        return self._model
+
     def add_scoring(self, scoring_function):
         self._scoring_hook.add(scoring_function)
 
@@ -21,7 +25,13 @@ class Pipeline(AbstractPipeline):
 
     def _pipeline(self):
         synthetic_data = ASyH.data.Data(data=self._model.synthesize())
-        return self._scoring_hook.execute(self._input_data, synthetic_data)
+        detailed_scores = self._scoring_hook.execute(self._input_data,
+                                                     synthetic_data)
+
+        print("Scoring: " + str(detailed_scores))
+        # Assuming, the scoring functions are maximizing, nomalized, and
+        # weighted equally:
+        return sum(detailed_scores.values()) / len(detailed_scores)
 
     def run(self):
         '''Start the pipeline'''
