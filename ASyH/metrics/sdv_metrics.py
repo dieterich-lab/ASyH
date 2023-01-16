85863e50b94bd13869b7ded2a276143d666d9e19
Tim Johann <tim.johann@phrogstar.de>
2023-01-16 14:42:50 +0100

    metrics/sdv_metrics.py: pass the actual metadata, not the Metadata object.

--- ASyH/metrics/sdv_metrics.py
+++ ASyH/metrics/sdv_metrics.py
@@ -6,6 +6,6 @@ def adapt_sdv_metric(sdv_metric_object):
     def closure(orig_data, synth_data):
         return sdv_metric_object.compute(orig_data.data,
                                          synth_data.data,
-                                         orig_data.metadata)
+                                         orig_data.metadata.metadata)
     closure.__name__ = sdv_metric_object.__name__
     return closure
