cf1156f8e74c5cec86d23716006cdfabef7aa094
Tim Johann <tim.johann@phrogstar.de>
2023-01-18 16:45:33 +0100

    sdv_metrics.py: add wrapper to explicitly normalize score.
      plus double-quotes for doc-strings.

--- ASyH/metrics/sdv_metrics.py
+++ ASyH/metrics/sdv_metrics.py
@@ -1,11 +1,26 @@
-'''Wrapper functions for SDV metrics'''
+"""Wrapper functions for SDV metrics."""
 
 
-def adapt_sdv_metric(sdv_metric_object):
-    '''Return a function to apply with the metric hook of ASyH.'''
+def adapt_sdv_metric(sdv_metric_class):
+    """Wrap SDV metric classes methods .compute(), return a function to apply
+    with the metric hook of ASyH.
+    """
     def closure(orig_data, synth_data):
-        return sdv_metric_object.compute(orig_data.data,
-                                         synth_data.data,
-                                         orig_data.metadata.metadata)
-    closure.__name__ = sdv_metric_object.__name__
+        return sdv_metric_class.compute(orig_data.data,
+                                        synth_data.data,
+                                        orig_data.metadata.metadata)
+    closure.__name__ = sdv_metric_class.__name__
+    return closure
+
+
+def adapt_sdv_metric_normalized(sdv_metric_class):
+    """Wrap SDV metric classes methods .compute() and .normalize(), returning a
+    function to apply with the metric hook of ASyH.
+    """
+    def closure(orig_data, synth_data):
+        raw_score = sdv_metric_class.compute(orig_data.data,
+                                             synth_data.data,
+                                             orig_data.metadata.metadata)
+        return sdv_metric_class.normalize(raw_score)
+    closure.__name__ = sdv_metric_class.__name__
     return closure
