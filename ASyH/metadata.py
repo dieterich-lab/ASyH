f061bfacd465da9b54a3c62b9c123f0cfb58a23e
Tim Johann <tim.johann@phrogstar.de>
2023-01-23 01:01:58 +0100

    metadata.py:  handle correct metadata format, with variable table names.
       SDV metadata is supposed to be formatted
       {"tables":
         {"tablename":
           {"fields:
             ...field: type/subtype definitions...
           }
         }
       }
    
       while the hierarchy names "tables" and "fields" are constants,
       the "tablename" can be exchange by any name.
    
       This patch handles the variability of the table names.
    
       The properties
         fields
       and
         table
       are introduced.  The latter the actual name of the table (in the
       case of multiple tables, the zeroth table's name).

--- ASyH/metadata.py
+++ ASyH/metadata.py
@@ -1,18 +1,38 @@
 # ASyH's simple metadata inference
 
+import pdb
+
 import pathlib
 import json
 from typing import Optional, Union, Dict, Any
 
 from pandas import DataFrame
 
+from ASyH.dataerror import DataError
+
 
 class Metadata:
+    'A wrapper class for SDV metadata.'
+
     def _skeleton(self, data):
         return
 
     def _create_skeleton_file(self, data):
         return
+
+    @property
+    def fields(self):
+        'Property method for retrieving the \'fields\' entry in the metadata.'
+        if self._get_set_tablename() is None:
+            return None
+        return self.metadata['tables'][self._tablename]['fields']
+
+    @property
+    def table(self):
+        'Property method for retrieving the (zeroth) \'tables\' entry in the metadata.'
+        if self._get_set_tablename() is not None:
+            return self.metadata['tables'][self._tablename]
+        return None
 
     def read(self, filename: Union[str, pathlib.Path]):
         '''Read the metadata from file into the metadata dict.'''
@@ -21,6 +41,8 @@ class Metadata:
                 self.metadata = json.load(f)
         else:
             raise FileNotFoundError
+        self._validate_metadata(self.metadata)
+        self._tablename = self._get_set_tablename()
 
     def save(self, out_filename: str):
         '''Save the metadata dict to json file.'''
@@ -31,7 +53,8 @@ class Metadata:
     def variables_by_type(self, type_string):
         '''Filter the dataset's variables by the string in their type
         information.'''
-        field_types = self.metadata['tables']['data']['fields']
+        assert self._tablename is not None
+        field_types = self.fields
         return [key  # the key, i.e. variable name
                 for key, typeinfo in field_types.items()
                 if typeinfo['type'] == type_string]
@@ -40,6 +63,11 @@ class Metadata:
         # for now just use the dataFrame's dtypes:
         if data is not None:
             self.metadata = data.dtypes.to_dict()
+            self._validate_metadata(self.metadata)
+            self._tablename = self._get_set_tablename()
+        else:
+            self.metadata = None
+            self._tablename = None
 
     def _infer(self, data_column):
         # ToDo!
@@ -49,3 +77,20 @@ class Metadata:
     def _infer_metadata(self, data_frame):
         meta = {x: self._infer(data_frame[x]) for x in data_frame.columns}
         return meta
+
+    def _validate_metadata(self, metadata: Dict[str, Any]):
+        if 'tables' not in metadata.keys():
+            raise DataError('Metadata malformed: no \'tables\' entry in outermost scope.')
+
+        if len(metadata['tables']) > 1:
+            Warning('Metadate describes a multitable dataset. ASyH works only \
+            with single table datasets.\n Using table' + list(metadata['tables'].keys())[0])
+
+    def _get_set_tablename(self):
+        if self._tablename is not None:
+            return self._tablename
+        if self.metadata is None:
+            return None
+        self._validate_metadata(self.metadata)
+        self._tablename = list(self.metadata['tables'].keys())[0]
+        return self._tablename
