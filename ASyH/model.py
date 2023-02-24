318126a6e81bb6c22887872b35aa33b1edce9a29
Tim Johann <tim.johann@phrogstar.de>
2023-02-24 17:47:11 +0100

    model.py: save/load of an ASyH.model.Model using pickle.

--- ASyH/model.py
+++ ASyH/model.py
@@ -5,9 +5,12 @@
 
 from datetime import datetime
 import os.path
+import pickle
 from typing import Optional, Callable, Any, Dict
 
+from typing_extensions import Self
 from pandas import DataFrame
+
 from sdv.tabular.base import BaseTabularModel
 
 from ASyH.data import Data
@@ -57,24 +60,33 @@ class Model:
         self._trained = True
 
     def save(self, filename: Optional[str] = None):
-        '''Save the SDV model to pkl.'''
-        if not self._sdv_model:
-            return
+        '''Save the ASyH model to pkl.'''
+        assert self._sdv_model is not None
+
+        if not self._trained:
+            Warning('Model has not been trained, saving is discouraged!')
+
         if not filename:  # create a filename from model type and date/time
             filename = self._model_type \
                 + str(datetime.now().isoformat(timespec='auto'))
+            Warning(f'No filename given, saving to {filename}.pkl.')
+
         if not filename.endswith('.pkl'):
             filename = filename + '.pkl'
-        self._sdv_model.save(filename)
 
-    def read(self, input_filename: str):
-        '''Read the SDV model from pkl.'''
+        with open(filename, 'wb') as out_file:
+            pickle.dump(self, out_file)
+
+    @classmethod
+    def read(cls: Self, input_filename: str) -> Self:
+        '''Read the ASyH model from pkl.'''
         # does filename exist?
         if not os.path.exists(input_filename):
             Warning('Model input file not found!')
-            return
-        if self._sdv_model:
-            self._sdv_model.read(input_filename)
+            return None
+        with open(input_filename, 'rb') as input_file:
+            model = pickle.load(input_file)
+        return model
 
     def synthesize(self, sample_size: int = -1) -> DataFrame:
         '''Create synthetic data.'''
@@ -82,7 +94,7 @@ class Model:
             self._train(None)
         if sample_size == -1:
             sample_size = self._input_data_size
-        return self.sdv_model.sample(sample_size)
+        return self._sdv_model.sample(sample_size)
 
     def adapted_arguments(self, data: Optional[Data] = None) -> Dict[str, Any]:
         '''Method for specific models to adapt the constructor arguments to
