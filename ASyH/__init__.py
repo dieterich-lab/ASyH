35637803feffb9cb568d4a32360c690e39a3dd45
Tim Johann <tim.johann@phrogstar.de>
2023-03-01 17:19:36 +0100

    __init__.py: pull classes and functions into root namespace.

---
+++ ASyH/__init__.py
@@ -0,0 +1,30 @@
+"""ASyH classes and utilities"""
+
+from ASyH.App import Application
+from ASyH.data import Data, SyntheticData
+from ASyH.metadata import Metadata
+from ASyH.model import Model
+from ASyH.models import TVAEModel, CTGANModel, CopulaGANModel, GaussianCopulaModel
+from ASyH.pipeline import Pipeline
+from ASyH.pipelines import TVAEPipeline, CTGANPipeline, CopulaGANPipeline, GaussianCopulaPipeline
+from ASyH.report import report
+from ASyH.dispatch import concurrent_dispatch
+
+__all__ = [
+    'Application',
+    'Data',
+    'SyntheticData',
+    'Metadata',
+    'Model',
+    'TVAEModel',
+    'CTGANModel',
+    'CopulaGANModel',
+    'GaussianCopulaModel',
+    'Pipeline',
+    'TVAEPipeline',
+    'CTGANPipeline',
+    'CopulaGANPipeline',
+    'GaussianCopulaPipeline',
+    'report',
+    'concurrent_dispatch'
+]
