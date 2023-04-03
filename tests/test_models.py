614a967952ea260b6f2d842aad53919abae22a0d
Tim Johann <tim.johann@phrogstar.de>
2023-04-03 12:49:11 +0200

    tests: update for SDV 1.0.0
      SDV's models were renamed (*Synthesizer)
      Fixture needs to source metadata (seems to be mandatory to specify
      metadata argument to *Synthesizers in sdv 1.0.0)

--- tests/test_models.py
+++ tests/test_models.py
@@ -15,13 +15,17 @@ def input_data():
         input_file = join(dirname(dirname(__file__)), 'examples', 'Kaggle_Sirio_Libanes-16features.xlsx')
         _input_data_cache = ASyH.data.Data()
         _input_data_cache.read(input_file)
+        metadata_file = join(dirname(dirname(__file__)), 'examples', 'Kaggle_Sirio_Libanes-16features.json')
+        _metadata = ASyH.metadata.Metadata()
+        _metadata.read(metadata_file)
+        _input_data_cache.set_metadata(_metadata)
     yield _input_data_cache
 
 
 def test_construct_tvae_model():
     """Testing initialization of the TVAE model"""
     m = ASyH.models.TVAEModel()
-    assert m.model_type == 'TVAE'
+    assert m.model_type == 'TVAESynthesizer'
     # private state:
     assert m._trained is False
     assert m._training_data is None
@@ -46,7 +50,7 @@ def test_train_synthesize_tvae_model(input_data):
 def test_construct_ctgan_model():
     """Testing initialization of the CTGAN model"""
     m = ASyH.models.CTGANModel()
-    assert m.model_type == 'CTGAN'
+    assert m.model_type == 'CTGANSynthesizer'
     # private state:
     assert m._trained is False
     assert m._training_data is None
@@ -71,7 +75,7 @@ def test_train_synthesize_ctgan_model(input_data):
 def test_construct_copula_gan_model():
     '''Testing initialization of the CopulaGAN model'''
     m = ASyH.models.CopulaGANModel()
-    assert m.model_type == 'CopulaGAN'
+    assert m.model_type == 'CopulaGANSynthesizer'
     # private state:
     assert m._trained is False
     assert m._training_data is None
@@ -96,7 +100,7 @@ def test_train_synthesize_copula_gan_model(input_data):
 def test_construct_gaussian_copula_model():
     """Testing initialization of the GaussianCopula model"""
     m = ASyH.models.GaussianCopulaModel()
-    assert m.model_type == 'GaussianCopula'
+    assert m.model_type == 'GaussianCopulaSynthesizer'
     # private state:
     assert m._trained is False
     assert m._training_data is None
