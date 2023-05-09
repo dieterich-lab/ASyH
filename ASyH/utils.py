9fc1f48c518ee0475ee18737e67e6b175515d9c9
Tim Johann <tim.johann@phrogstar.de>
2023-05-09 16:30:38 +0200

    utils.py:  function flatten_dict to have a tree be flattenend.
      Flattening will create a dictionary whose values are not
      dictionaries themselves.  key names of all these 'leaves' of the
      tree will be the data paths concatenated by a dot ('.'), e.g.
      {'a': {'b': {'d': 1, 'e': 2}, 'c': 3}}
      will be transformed into
      {'a.b.d': 1, 'a.b.e': 2, 'a.c': 3}.

--- ASyH/utils.py
+++ ASyH/utils.py
@@ -1,6 +1,21 @@
 import inspect
+from typing import Dict, Any
 
 
 def ToDo():
     print(inspect.currentframe().f_back.f_code.co_name +
           "This feature is not implemented yet.", end='\n')
+
+
+def flatten_dict(indict: Dict[str, Any],
+                 root_key: str = '',
+                 key_separator: str = '.'):
+    items = []
+    for key, val in indict.items():
+        context = root_key + key_separator + key if root_key else key
+        if isinstance(val, dict):
+            items.extend(
+                flatten_dict(val, context, key_separator=key_separator).items())
+        else:
+            items.append((context, val))
+    return dict(items)
