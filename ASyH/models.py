2b94b27d5610d8a0c73cefc9dad0ea8c2ddd10e0
Gherman Serghei <geron@gero.gero.net>
2024-10-18 14:05:34 +0200

    Added the model class CTABGAN to models.py, added additional base classto model.py, added modules ctabgan_synthesizer and transformer for ctabgan

--- ASyH/models.py
+++ ASyH/models.py
@@ -10,7 +10,9 @@ import rdt
 
 import sdv
 from ASyH.data import Data
-from ASyH.model import Model
+from ASyH.model import Model, ModelX
+from ASyH.ctabgan_synthesiter import *
+from ASyH.transformer_ctabgan import *
 
 
 class TVAEModel(Model):
@@ -146,6 +148,42 @@ class GaussianCopulaModel(Model):
 
         return {'numerical_distributions': column_distributions,
                 'default_distribution': best_scores['categorical'][0]}
+
+
+class CTABGAN():
+    def __init__(self,
+                 pd_data,
+                 categorical_columns = [], 
+                 log_columns = [],
+                 mixed_columns= {},
+                 general_columns = [],
+                 non_categorical_columns = [],
+                 integer_columns = []):
+        self.synthesizer = CTABGANSynthesizer()
+        self.raw_df = pd_data
+        self.categorical_columns = categorical_columns
+        self.log_columns = log_columns
+        self.mixed_columns = mixed_columns
+        self.general_columns = general_columns
+        self.non_categorical_columns = non_categorical_columns
+        self.integer_columns = integer_columns
+
+    def fit(self):
+
+        start_time = time.time()
+        self.data_prep = DataPrep(self.raw_df,self.categorical_columns,self.log_columns,self.mixed_columns,self.general_columns,self.non_categorical_columns,self.integer_columns)
+        self.synthesizer.fit(train_data=self.data_prep.df, categorical = self.data_prep.column_types["categorical"], mixed = self.data_prep.column_types["mixed"],
+        general = self.data_prep.column_types["general"], non_categorical = self.data_prep.column_types["non_categorical"])
+        end_time = time.time()
+        print('Finished training in',end_time-start_time," seconds.")
+
+
+    def generate_samples(self):
+
+        sample = self.synthesizer.sample(len(self.raw_df))
+        sample_df = self.data_prep.inverse_prep(sample)
+
+        return sample_df
 
 
 def _init_iterative_scores(metadata_dict: Dict[str, any]):
